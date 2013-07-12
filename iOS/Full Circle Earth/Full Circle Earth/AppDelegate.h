5a1271ce3ef57ad81d9620da83bab397ad1242e9
Muthu Arumugam <muthu.kumar@me.com>
2013-07-12 12:57:57 -0400

    Empty projects

---
+++ iOS/Full Circle Earth/Full Circle Earth/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Full Circle Earth
+//
+//  Created by marumugam on 7/12/13.
+//  Copyright (c) 2013 MK Touch Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
